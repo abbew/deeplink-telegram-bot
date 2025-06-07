56d271e6d2b19dae140a89157852b5b830573992
abbew <abbew.xtgem@googlemail.com>
2025-06-07 15:21:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 const TelegramBot = require('node-telegram-bot-api');
 
-const token = '8045648382:AAFVn8WDMiWU96FEIa1UjS5z-ZpE4kkgheM';
+const token = process.env.BOT_TOKEN;
 const bot = new TelegramBot(token, { polling: true });
 
 console.log('🤖 Bot sedang berjalan...');
